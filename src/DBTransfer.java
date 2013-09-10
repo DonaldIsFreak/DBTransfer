36bbaedb2f0348af25dff8b1ef6a32f91cdad3cf
DonaldIsFreak <ron81018@gmail.com>
2013-09-10 14:51:39 +0800

    Modify the DBTransfer

--- src/DBTransfer.java
+++ src/DBTransfer.java
@@ -2,6 +2,7 @@ import java.io.InputStream;
 import java.io.IOException;
 import java.io.FileInputStream;
 import java.util.Map;
+import java.util.List;
 import java.util.HashMap;
 import java.util.Properties;
 
@@ -15,30 +16,45 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class DBTransfer{
 	private Map<String,String> configs = new HashMap<String,String>();
+	private String db_type;
 	private String resource = "transfer-config.xml";
 
-	public DBTransfer(){
+	public DBTransfer(String db_type){
 		// Create a Dict to store file name.
 		configs.put("source","source_config.properties");
 		configs.put("target","target_config.properties");
+		this.db_type = db_type;
 	}
-	
-	public SqlSession getSession(String name) throws IOException{
-		Properties props = new Properties();
-		props.load(new FileInputStream(configs.get(name)));
-		InputStream reader = Resources.getResourceAsStream(resource);
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader,props);
-		return sqlSessionFactory.openSession();
-	} 
 
-	public static void main(String[] argv) throws Exception{
-		DBTransfer transfer = new DBTransfer();
-		SqlSession session = transfer.getSession("source");
+	public SqlSession getSession(){
+		return getSession(this.db_type);
+	}	
+
+	public SqlSession getSession(String name){
+		Properties props = new Properties();
 		try{
-			User user = session.selectOne("UserMapper.selectUser",1);
-			System.out.println(user.getId());
-		}finally{
+			props.load(new FileInputStream(configs.get(name)));
+			InputStream reader = Resources.getResourceAsStream(resource);
+			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader,props);
+			return sqlSessionFactory.openSession();
+		}catch(IOException e){
+		}
+		return null;
+	} 
+	public User getUserByID(int id){
+		SqlSession session= getSession();
+		User user=null;
+		try{
+			user = session.selectOne("UserMapper.getUserByID",id);
+		}finally {
 			session.close();
 		}
+		return user;
+	}	
+
+	public static void main(String[] argv) throws Exception{
+		DBTransfer transfer = new DBTransfer("source");
+		User user = transfer.getUserByID(1);	
+		System.out.println(user);
 	}
 }
