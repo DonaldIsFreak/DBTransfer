7bd5e05ebdca01b7bf005e5fb12b265f1de4019d
DonaldIsFreak <ron81018@gmail.com>
2013-09-11 10:11:44 +0800

    Modify the SqlSession to be Singleton.

--- src/DBTransfer.java
+++ src/DBTransfer.java
@@ -1,12 +1,10 @@
-import java.io.InputStream;
-import java.io.IOException;
-import java.io.FileInputStream;
 import java.util.Map;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Properties;
-
-import org.apache.log4j.Logger;
+import java.io.InputStream;
+import java.io.IOException;
+import java.io.FileInputStream;
 
 import org.apache.ibatis.io.Resources; 
 import org.apache.ibatis.session.SqlSession; 
@@ -17,6 +15,7 @@ public class DBTransfer{
 	private Map<String,String> configs = new HashMap<String,String>();
 	private String db_type;
 	private String resource = "transfer-config.xml";
+	private SqlSession session = null;
 
 	public DBTransfer(String db_type){
 		// Create a Dict to store file name.
@@ -28,37 +27,35 @@ public class DBTransfer{
 	public SqlSession getSession(){
 		return getSession(this.db_type);
 	}	
-public SqlSession getSession(String name){
+
+	public SqlSession getSession(String name){
+		if (this.session != null)
+			return this.session;
+
 		Properties props = new Properties();
 		try{
 			props.load(new FileInputStream(configs.get(name)));
 			InputStream reader = Resources.getResourceAsStream(resource);
 			SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader,props);
-			return sqlSessionFactory.openSession();
+			this.session = sqlSessionFactory.openSession();
 		}catch(IOException e){
+			System.out.println(e.getMessage());
 		}
-		return null;
+		return this.session;
 	} 
 
 	public User getUserByID(int id){
 		SqlSession session= getSession();
 		User user=null;
-		try{
-			user = session.selectOne("UserMapper.getUserByID",id);
-		}finally {
-			session.close();
-		}
+		user = session.selectOne("UserMapper.getUserByID",id);
+
 		return user;
 	}	
 	
 	public List<User> getAllUsers(){
 		SqlSession session = getSession();
 		List<User> users = null;
-		try {
-			users = session.selectList("UserMapper.getAllUsers");
-		}finally {
-			session.close();
-		}
+		users = session.selectList("UserMapper.getAllUsers");
 
 		return users;
 	}
@@ -66,56 +63,41 @@ public SqlSession getSession(String name){
 	public List<User> getUserRange(int start,int end){
 		SqlSession session = getSession();
 		List<User> users = null;
-		try {
-			HashMap<String,Integer> range = new HashMap<String,Integer>();
-			range.put("start",start);
-			range.put("end",end);
-			users = session.selectList("UserMapper.getUserRange",range);
-		}finally {
-			session.close();
-		}
+		HashMap<String,Integer> range = new HashMap<String,Integer>();
+		range.put("start",start);
+		range.put("end",end);
+		users = session.selectList("UserMapper.getUserRange",range);
+
 		return users;
 	}
 
 	public void setUsers(List<User> users){
 		SqlSession session = getSession();
-		try {
-			for(User user:users){
-				session.insert("UserMapper.setUser",user);
-				System.out.println(user.getId());
-			}			
-			User startUser = users.get(0);
-			User endUser = users.get(users.size()-1);
-			int startNo = startUser.getId();
-			int endNo = endUser.getId();
-			Log log = new Log();
-			log.setStartNo(startNo);
-			log.setEndNo(endNo);
-			session.insert("LogMapper.setLog",log);
-			session.commit();
-		} finally {
-			session.close();
-		}
+		for(User user:users){
+			session.insert("UserMapper.setUser",user);
+			System.out.println(user.getId());
+		}			
+		User startUser = users.get(0);
+		User endUser = users.get(users.size()-1);
+		int startNo = startUser.getId();
+		int endNo = endUser.getId();
+		Log log = new Log();
+		log.setStartNo(startNo);
+		log.setEndNo(endNo);
+		session.insert("LogMapper.setLog",log);
+		session.commit();
 	}
 
 	public void setUser(User user){
 		SqlSession session = getSession();
-		try{
-			session.insert("UserMapper.setUser",user);
-			session.commit();
-		}finally {
-			session.close();
-		}
+		session.insert("UserMapper.setUser",user);
+		session.commit();
 	}
 
 	public void setLog(Log log){
 		SqlSession session = getSession();
-		try {
-			session.insert("LogMapper.setLog",log);
-			session.commit();
-		}finally {
-			session.close();
-		}
+		session.insert("LogMapper.setLog",log);
+		session.commit();
 	}
 
 	public int getLastNo(){
@@ -125,29 +107,21 @@ public SqlSession getSession(String name){
 			result = session.selectOne("LogMapper.getLog");
 		}catch(Exception e){
 			result =0;
-		} finally {
-			session.close();
-		}
+		} 
 
 		return result;	
+	}
+
+	public void close(){
+		if (this.session != null)
+			this.session.close();
 	}
 
 	public static void main(String[] argv) throws Exception{
 		int COUNT = 10;
 		DBTransfer source = new DBTransfer("source");
 		DBTransfer target = new DBTransfer("target");
-		/* Test Code
-		User user = source.getUserByID(1);	
-		System.out.println(user);
-		List<User> users = source.getAllUsers();
-		target.setUsers(users);
-		Log log = new Log();
-		log.setStartNo(1);
-		log.setEndNo(5);
-		System.out.println(log);
-		target.setLog(log);
-		System.out.println(target.getLastNo());
-		*/
+
 		int lastNo = target.getLastNo();	
 		int startNo = lastNo+1;
 		int endNo = startNo+COUNT;	
@@ -156,7 +130,10 @@ public SqlSession getSession(String name){
 		for (User user: users){
 			System.out.println(user.getId());	
 		}	
-
-		target.setUsers(users);
+		
+		if (users.size()>0)
+			target.setUsers(users);
+		source.close();
+		target.close();
 	}
 }
