208e02b3ce46bcbaa02c7697a7c0a8855b0d3383
DonaldIsFreak <ron81018@gmail.com>
2013-09-18 12:33:56 +0800

    Change the DB Driver from MySQL to MS SQL

--- data/run.sh
+++ data/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-java -cp .:mysql-connector-java.jar:mybatis-3.2.2.jar:log4j-1.2.17.jar DBTransfer 2> run.log 
+java -cp .:mysql-connector-java.jar:mybatis-3.2.2.jar:log4j-1.2.17.jar:sqljdbc4.jar DBTransfer 2> run.log 
 
